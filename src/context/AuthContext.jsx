4ba50fa025d00d8a82f182bd71988c2ada9c29e2
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2024-12-20 20:30:28 +0530

    add: auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext,useState, useContext, useEffect } from "react";
+import account from "../services/appwrite.config";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const checkSession = async () => {
+            try {
+                const session = await account.get();
+                setUser(session);
+            } catch (error) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+        checkSession();
+    },[]);
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}    
+
+export const useAuth = () => useContext(AuthContext);
