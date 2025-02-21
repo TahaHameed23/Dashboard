dc3d5e0c82d2bb119fbef75e42e2cbfce4fd56fc
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-02-22 02:43:05 +0530

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,36 +1,34 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState, useContext, useEffect } from "react";
-import { account } from "../services/appwrite.config";
-import { Navigate } from "react-router-dom";
+import { createContext,useState, useContext, useEffect } from "react";
+import account from "../services/appwrite.config";
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const excludedRoutes = ["/login", "/login/reset-password", "/signup"];
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    if (excludedRoutes.includes(window.location.pathname)) {
-      setLoading(false);
-      return;
-    }
-    const checkSession = async () => {
-      try {
-        const session = await account.get();
-        setUser(session);
-      } catch {
-        setUser(null);
-        <Navigate to={"/login"} />;
-      } finally {
-        setLoading(false);
-      }
-    };
-    checkSession();
-  }, []);
+    useEffect(() => {
+        const checkSession = async () => {
+            try {
+                const session = await account.get();
+                setUser(session);
+            } catch {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+        checkSession();
+    },[]);
 
-  return <AuthContext.Provider value={{ user, setUser, loading }}>{children}</AuthContext.Provider>;
-};
+    return (
+        <AuthContext.Provider value={{ user, setUser, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}    
 
 export const useAuth = () => {
-  return useContext(AuthContext);
-};
+    return useContext(AuthContext);
+}
