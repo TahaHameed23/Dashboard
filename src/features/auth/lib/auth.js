11e1b4b6bfafd9672deab5c69bd586c8efabf126
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2025-02-22 16:49:39 +0530

    add facade fetch func

--- src/features/auth/lib/auth.js
+++ src/features/auth/lib/auth.js
@@ -1,18 +1,11 @@
-const API_URL = decodeURIComponent(import.meta.env.VITE_API_URL.trim());
+import { post } from "../../../lib/fetch";
 
 export const sendNewClientData = async (session) => {
     const sessionData = {
         "id":session.$id,
         "email":session.email
     }
-    await fetch(`${API_URL}/auth/client/create`,
-    {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify(sessionData),
-    });
+    const response = await post("/auth/client/create", sessionData);
+    return response;
      
  }
